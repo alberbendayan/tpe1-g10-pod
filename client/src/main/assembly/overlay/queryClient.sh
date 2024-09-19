a04176e48537298f71558265b05e5615cd49613f
bendeyhe <108757682+bendeyhe@users.noreply.github.com>
2024-09-19 15:56:04 -0300

    sh and clients
    Co-Authored-By: Alberto Bendayan <108437189+alberbendayan@users.noreply.github.com>
    Co-Authored-By: juan cruz boullosa gutierrez <108482692+boullo2002@users.noreply.github.com>

---
+++ client/src/main/assembly/overlay/queryClient.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+for i in "$@"; do
+    case $i in
+        -D*=*)
+        JAVA_OPTS="$JAVA_OPTS $i"
+        ;;
+        *)
+        break
+        ;;
+    esac
+done
+
+MAIN_CLASS="ar.edu.itba.pod.client.QueryClient"
+
+java $JAVA_OPTS -cp 'lib/jars/*'  $MAIN_CLASS $*
---
+++ client/src/main/java/ar/edu/itba/pod/client/AdministrationClient.java
@@ -0,0 +1,11 @@
+package ar.edu.itba.pod.client;
+
+public class AdministrationClient {
+    public static void main(String[] args) {
+        System.getProperty("action");
+        System.getProperty("doctor");
+        System.getProperty("level");
+        //ChannelCreator.createChannel(System.getProperty("serverAddress"));
+    }
+
+}
